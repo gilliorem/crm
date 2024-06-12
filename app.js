bc35289cfb5c2214dca5bab12fe75f856593d0db
Remi Gilliot <remi.gilliot98@gmail.com>
2024-06-12 23:05:19 +0200

    del console log 'hello'

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("cc");
 const windowApp = document.querySelector(".window-app");
 const connectionWindow = document.querySelector(".connection-window");
 const email = document.querySelector('.email-input');
